68e043c9bde12b8590cff654b49219dc15dc9c3f
Riyo-Aloshyas <42985877+Riyo-Aloshyas@users.noreply.github.com>
2020-05-06 14:31:34 -0500

    Update stream.py

--- stream.py
+++ stream.py
@@ -3,6 +3,7 @@ import socket
 import re
 # import emoji
 import preprocessor as p # pip install tweet-preprocessor
+import googlemaps
 
 # Enter your Twitter keys here!!!
 ACCESS_TOKEN = "1253174517255606272-7T6XvB4M7NFpwCePdNirpG4fi60FDK"
@@ -12,6 +13,8 @@ CONSUMER_SECRET = "R27hiG8mgy1xlAFzScyklrhgLIhkdKHIVNUf3Ww6Ucf7VJUQBm"
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+########################################
+gmaps = googlemaps.Client(key='AIzaSyAxEtQaX4yhn8dZfZQp9LQjW3noMB4iaRw')
 
 hashtag = '#covid19'
 
@@ -51,6 +54,26 @@ def getTweet(status):
 
     return location, preprocessing(tweet)
 
+###############################
+'''
+if status.user.location:
+            geo = self.gmaps.geocode(status.user.location)
+            if geo:
+                coordinate = geo[0]['geometry']['location']
+                location = str(coordinate['lng']) + "," + str(coordinate['lat'])
+            else:
+                location = ''
+        else:
+            location = ''
+
+        text = '|(' + location + ')' + text + '|'
+        print('===================')
+        print(text)
+        print('===================')
+        conn.send(text.encode('utf-8'))
+'''
+#################################
+
 
 # create sockets
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -63,12 +86,21 @@ class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         location, tweet = getTweet(status)
 
-        if (location != None and tweet != None):
-            tweetLocation = location + "::" + tweet+"\n"
-            print(status.text)
-            conn.send(tweetLocation.encode('utf-8'))
+        if status.user.location:
+            geo = self.gmaps.geocode(status.user.location)
+            if geo:
+                coordinate = geo[0]['geometry']['location']
+                location = str(coordinate['lng']) + "," + str(coordinate['lat'])
+            else:
+                location = ''
+        else:
+            location = ''
 
-        return True
+        text = '|(' + location + ')' + text + '|'
+        print('===================')
+        print(text)
+        print('===================')
+        conn.send(text.encode('utf-8'))
 
 
     def on_error(self, status_code):
